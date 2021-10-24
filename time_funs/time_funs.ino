2d12f38797fd9006a98d2c85c405d53908fea673
emmamack <42980990+emmamack@users.noreply.github.com>
2021-10-24 17:18:53 -0400

    Menu, meal time detecting, time centering

--- time_funs/time_funs.ino
+++ time_funs/time_funs.ino
@@ -11,8 +11,9 @@
 #define SCREEN_ADDRESS 0x3C
 #define WHITE SSD1306_WHITE
 #define BLACK SSD1306_BLACK
-Adafruit_SSD1306 display(SCREEN_WIDTH, SCREEN_HEIGHT, &Wire, OLED_RESET);
+#define BORDER 4
 
+Adafruit_SSD1306 display(SCREEN_WIDTH, SCREEN_HEIGHT, &Wire, OLED_RESET);
 RTC_DS3231 rtc;
 bool isDownA1 = false; bool wasPressedA1 = false;
 bool isDownA2 = false; bool wasPressedA2 = false;
@@ -24,6 +25,7 @@ struct Time {
     uint8_t second;
 };
 
+Time nextMealTime = {255, 255, 255};
 
 Time getCurrentTime() {
   DateTime now = rtc.now();
@@ -44,15 +46,25 @@ void displayPrintCenter(char* string, uint8_t cursorY, uint16_t color) {
 }
 
 void displayTime(Time time, uint8_t x, uint8_t y, uint8_t textSize) {
-  display.setCursor(x, y);
   display.setTextSize(textSize);
   display.setTextColor(WHITE); //TODO: don't hard code this
   
 //  display.fillRect(x, y, 25, 25, WHITE);
 //  display.setTextColor(BLACK);
 
+  int16_t  x1, y1;
+  uint16_t w, h;
+  if (time.hour % 12 > 9 || time.hour == 12 || time.hour == 0) {
+    display.getTextBounds("AAAAAAAA", 0, 0, &x1, &y1, &w, &h);
+  } else {
+    display.getTextBounds("AAAAAAA", 0, 0, &x1, &y1, &w, &h);
+  }
+  uint8_t offset = w/2;
+  int8_t cursorX = SCREEN_WIDTH/2 - offset;
+  display.setCursor(cursorX, y);
+
   uint8_t hour_rep = time.hour % 12;
-  if (time.hour == 12|| time.hour == 0) hour_rep = 12;
+  if (time.hour == 12 || time.hour == 0) hour_rep = 12;
   display.print(hour_rep); 
 //  display.setTextColor(BLACK);
   display.print(":");
@@ -71,13 +83,13 @@ void setCurrentTime(Time time) {
   rtc.adjust(DateTime(2000, 1, 1, time.hour, time.minute, time.second));
 }
 
-void saveFeedTime(Time time, int mealNum) {
+void saveFeedTime(Time time, uint8_t mealNum) {
   EEPROM.write(mealNum*3,   time.hour);
   EEPROM.write(mealNum*3+1, time.minute);
   EEPROM.write(mealNum*3+2, time.second);
 }
 
-Time getFeedTime(int mealNum) {
+Time getFeedTime(uint8_t mealNum) {
   Time time;
   time.hour   = EEPROM.read(mealNum*3);
   time.minute = EEPROM.read(mealNum*3+1);
@@ -85,10 +97,42 @@ Time getFeedTime(int mealNum) {
   return time;
 }
 
+uint16_t getMinutes(Time time) {
+  return 60*time.hour + time.minute;
+}
+
+void getNextMealTime() {
+  uint16_t meal1Mins = getMinutes(getFeedTime(0));
+  uint16_t meal2Mins = getMinutes(getFeedTime(1));
+  uint16_t currMins  = getMinutes(getCurrentTime());
+
+  if (meal1Mins <= currMins) meal1Mins += 1440;
+  if (meal2Mins <= currMins) meal2Mins += 1440;
+
+  if (((meal1Mins - currMins) < (meal2Mins - currMins)) || ((meal2Mins - currMins) == 1440)){
+    nextMealTime = getFeedTime(0);
+  } 
+  if (((meal2Mins - currMins) < (meal1Mins - currMins)) || ((meal1Mins - currMins) == 1440)) {
+    nextMealTime = getFeedTime(1);
+  }
+}
+
+void displayHomeScreen(Time nextMealTime) {
+  display.clearDisplay();
+  
+  Time currTime = getCurrentTime();
+  displayTime(currTime, 20, 10, 2);
+  
+  displayPrintCenter("Next meal time:", 35, WHITE);
+  displayTime(nextMealTime, 35, 47, 1);
+  
+  display.display();
+}
+
 void updateButtons() {
-  int A1Val = analogRead(A1);
-  int A2Val = analogRead(A2);
-  int A3Val = analogRead(A3);
+  uint16_t A1Val = analogRead(A1);
+  uint16_t A2Val = analogRead(A2);
+  uint16_t A3Val = analogRead(A3);
 
   if (wasPressedA1) wasPressedA1 = false;
   if (A1Val < 500) isDownA1 = true;
@@ -137,29 +181,24 @@ Time incrementTime(Time time, bool up, char timeSection) {
 }
 
 Time userSetTime(char* title) {
-  Serial.print("Title: "); Serial.println(title);
   Time setTime = {12, 0, 0};
-  char timeSections [3] = {'h','m','t'};
+  char timeSections [2] = {'h','m'};
   uint8_t timeSectionInd = 0;
 
   while(1) {
     updateButtons();
     if (wasPressedA1) {
-      Serial.println("A1 was pressed!");
       setTime = incrementTime(setTime, false, timeSections[timeSectionInd]);
     }
     
     if (wasPressedA2) {
-      Serial.println("A2 was pressed!");
       if (timeSectionInd >= 1) {
-        Serial.println("Back to main");
         return setTime;
       }
       timeSectionInd += 1;
     }
     
     if (wasPressedA3) {
-      Serial.println("A3 was pressed!");
       setTime = incrementTime(setTime, true, timeSections[timeSectionInd]);
     }
     
@@ -171,34 +210,56 @@ Time userSetTime(char* title) {
   }
 }
 
-void userChooseOptions(char* op1, char* op2) {
-  Serial.print("op1: "); Serial.println(op1);
-  Serial.print("op2: "); Serial.println(op2);
-  uint8_t border = 4;
-//  bool isHighlighted1 = true;
+bool userChooseOptions(char* op1, char* op2) {
+  bool isHighlighted1 = true; //TODO: this is a little hacky
   
   while(1) {
     display.clearDisplay();
-//    updateButtons();
+    updateButtons();
 
-//    if (wasPressedA1 || wasPressedA3) isHighlighted1^true;
-
-//    if (isHighlighted1) {
-      Serial.println("1 is highlighted");
-      display.fillRect(border, border, SCREEN_WIDTH - 2*border, SCREEN_HEIGHT/2 - border, WHITE);
+    if (wasPressedA1 || wasPressedA3) isHighlighted1^=true;
+    
+    if (isHighlighted1) {
+      display.fillRect(BORDER, BORDER, SCREEN_WIDTH - 2*BORDER, 
+                       SCREEN_HEIGHT/2 - BORDER, WHITE);
       displayPrintCenter(op1, SCREEN_HEIGHT/4, BLACK);
-      display.drawRect(border, (SCREEN_HEIGHT+border)/2, SCREEN_WIDTH - 2*border, SCREEN_HEIGHT/2 - border, WHITE);
+      display.drawRect(BORDER, (SCREEN_HEIGHT+BORDER)/2, SCREEN_WIDTH - 2*BORDER, 
+                       SCREEN_HEIGHT/2 - BORDER, WHITE);
       displayPrintCenter(op2, SCREEN_HEIGHT*3/4, WHITE);
-//    } else {
-//      Serial.println("2 is highlighted");
-//    }
+      
+    } else {
+      display.drawRect(BORDER, BORDER, SCREEN_WIDTH - 2*BORDER, 
+                       SCREEN_HEIGHT/2 - BORDER, WHITE);
+      displayPrintCenter(op1, SCREEN_HEIGHT/4, WHITE);
+      display.fillRect(BORDER, (SCREEN_HEIGHT+BORDER)/2, SCREEN_WIDTH - 2*BORDER, 
+                       SCREEN_HEIGHT/2 - BORDER, WHITE);
+      displayPrintCenter(op2, SCREEN_HEIGHT*3/4, BLACK);
+    }
+
+    if (wasPressedA2) {
+      return isHighlighted1;
+    }
     
     display.display();
     delay(30);
   }
 }
 
-void setup () {
+void printTime(Time time) {
+  Serial.print(time.hour);
+  Serial.print(":");
+  Serial.println(time.minute);
+}
+
+void checkIfFeedTime() {
+  if (getMinutes(nextMealTime) == getMinutes(getCurrentTime())) {
+    Serial.println("TIME TO EAT!");
+    // rotateTray();
+    delay(1001); //TODO: don't need this once motor rotation exists
+  }
+}
+
+void setup() {
   pinMode(A1, INPUT_PULLUP);
   pinMode(A2, INPUT_PULLUP);
   pinMode(A3, INPUT_PULLUP);  
@@ -208,56 +269,49 @@ void setup () {
   while (!Serial); // wait for serial port to connect. Needed for native USB
   #endif
   if (! rtc.begin()) {
-    Serial.println("Couldn't find RTC");
+    Serial.println("RTC f");
     Serial.flush();
     abort();
   }
   if (rtc.lostPower()) {
-    Serial.println("RTC lost power, need to get time from the user");
+    Serial.println("RTC p");
     rtc.adjust(DateTime(F(__DATE__), F(__TIME__)));
   }
   // SSD1306_SWITCHCAPVCC = generate display voltage from 3.3V internally
   if(!display.begin(SSD1306_SWITCHCAPVCC, SCREEN_ADDRESS)) {
-    Serial.println(F("SSD1306 allocation failed"));
+    Serial.println(F("Al F"));
     for(;;); // Don't proceed, loop forever
   }
 }
 
-
-void loop () {
-  Time currTime = getCurrentTime();
-  display.clearDisplay();
-  
-  displayTime(currTime, 20, 10, 2);
-  
-  Time breakfastTime = getFeedTime(0);
-  display.setCursor(25, 30);
-  char bTime[] = "Breakfast time:";
-  displayPrintCenter(bTime, 30, WHITE);
-  displayTime(breakfastTime, 35, 45, 1);
-  
-  display.display();
+void loop() {
+  checkIfFeedTime();
+  getNextMealTime();
+  Serial.print("next "); printTime(nextMealTime);
+  Serial.print("1 "); printTime(getFeedTime(0));
+  Serial.print("2 "); printTime(getFeedTime(1));
+  displayHomeScreen(nextMealTime);
   updateButtons();
 
-  if (wasPressedA2) {
-    Serial.println("Going to choose options");
-    char op1[] = "Choose meal times";
-    char op2[] = "Set current time";
-    userChooseOptions(op1, op2);
-  }
-  
-  if (wasPressedA3) { 
-    Serial.println("Going to set time mode");
-    char title[] = "Set current time";
-    Time setTime = userSetTime(title);
-    setCurrentTime(setTime);
-  }
+  if (wasPressedA1 || wasPressedA2 || wasPressedA3) {
+    bool menuSelection = userChooseOptions("Choose meal times", "Set current time");
+    
+    if (menuSelection) {
+      bool mealSelection = userChooseOptions("Meal 1", "Meal 2");
+      
+      if (mealSelection) {
+        Time meal1Time = userSetTime("Set meal 1 time");
+        saveFeedTime(meal1Time, 0);
 
-  if (wasPressedA1) {
-    Serial.println("Setting breakfast time");
-    char title[] = "Set breakfast time";
-    Time breakfastTime = userSetTime(title);
-    saveFeedTime(breakfastTime, 0);
+      } else {
+        Time meal2Time = userSetTime("Set meal 2 time");
+        saveFeedTime(meal2Time, 1);
+      }
+      
+    } else {
+      Time setTime = userSetTime("Set current time");
+      setCurrentTime(setTime);
+    }
   }
   
   delay(30);
