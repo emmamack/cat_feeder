babd3451db8d511d77a42f4533a53f67eb27eb25
emmamack <42980990+emmamack@users.noreply.github.com>
2021-11-02 17:31:37 -0400

    Stepper homing, rotating at feed time

--- time_funs/time_funs.ino
+++ time_funs/time_funs.ino
@@ -3,6 +3,7 @@
 #include <Wire.h>
 #include <Adafruit_GFX.h>
 #include <Adafruit_SSD1306.h>
+#include <SpeedyStepper.h>
 #include <EEPROM.h>
 
 #define SCREEN_WIDTH 128 
@@ -13,11 +14,24 @@
 #define BLACK SSD1306_BLACK
 #define BORDER 4
 
+#define HOME_PIN A7
+#define UP_PIN A3
+#define MID_PIN A2
+#define DOWN_PIN A1
+#define MOTOR_STEP_PIN 6
+#define MOTOR_DIRECTION_PIN 5
+#define M0_PIN 9
+#define M1_PIN 8
+#define M2_PIN 7
+#define EN_PIN 10
+
 Adafruit_SSD1306 display(SCREEN_WIDTH, SCREEN_HEIGHT, &Wire, OLED_RESET);
 RTC_DS3231 rtc;
-bool isDownA1 = false; bool wasPressedA1 = false;
-bool isDownA2 = false; bool wasPressedA2 = false;
-bool isDownA3 = false; bool wasPressedA3 = false;
+SpeedyStepper stepper;
+
+bool isDownUP = false;   bool wasPressedUP = false;
+bool isDownMID = false;  bool wasPressedMID = false;
+bool isDownDOWN = false; bool wasPressedDOWN = false;
 
 struct Time {
     uint8_t hour;
@@ -26,6 +40,7 @@ struct Time {
 };
 
 Time nextMealTime = {255, 255, 255};
+bool blinkOn = false; uint8_t blinkCounter = 0;
 
 Time getCurrentTime() {
   DateTime now = rtc.now();
@@ -45,37 +60,69 @@ void displayPrintCenter(char* string, uint8_t cursorY, uint16_t color) {
   
 }
 
-void displayTime(Time time, uint8_t x, uint8_t y, uint8_t textSize) {
-  display.setTextSize(textSize);
-  display.setTextColor(WHITE); //TODO: don't hard code this
-  
-//  display.fillRect(x, y, 25, 25, WHITE);
-//  display.setTextColor(BLACK);
-
-  int16_t  x1, y1;
-  uint16_t w, h;
-  if (time.hour % 12 > 9 || time.hour == 12 || time.hour == 0) {
-    display.getTextBounds("AAAAAAAA", 0, 0, &x1, &y1, &w, &h);
-  } else {
-    display.getTextBounds("AAAAAAA", 0, 0, &x1, &y1, &w, &h);
+void handleBlinks(uint8_t numCycles) {
+  if (blinkCounter >= numCycles) {
+    blinkOn ^= true;
+    blinkCounter = 0;
   }
-  uint8_t offset = w/2;
-  int8_t cursorX = SCREEN_WIDTH/2 - offset;
-  display.setCursor(cursorX, y);
+  blinkCounter += 1;
+}
 
+void displayTime(Time time, uint8_t cursorY, uint8_t textSize, uint8_t highlight, bool blinking) {
+  
+  display.setTextColor(WHITE);
+  
+  display.setTextSize(textSize);
+  int16_t  x1, y1;
+  uint16_t wUnit, hUnit;
+  display.getTextBounds("0", 0, 0, &x1, &y1, &wUnit, &hUnit);
+  uint8_t wHighlight = wUnit*2 + 2;
+  int8_t cursorX = SCREEN_WIDTH/2 - wUnit/2*8;
+  if ((time.hour % 12 < 10) && (time.hour != 0) && (time.hour != 12)) {
+    cursorX = SCREEN_WIDTH/2 - wUnit/2*7;
+    wHighlight = wUnit + 2;
+  }
+  display.setCursor(cursorX, cursorY);
+
+
+  if (highlight == 1) {
+    display.setTextColor(BLACK);
+    display.fillRect(cursorX - 2, cursorY - 2, wHighlight, hUnit + 2, WHITE);
+  }
   uint8_t hour_rep = time.hour % 12;
   if (time.hour == 12 || time.hour == 0) hour_rep = 12;
   display.print(hour_rep); 
-//  display.setTextColor(BLACK);
-  display.print(":");
+  if (highlight == 1) display.setTextColor(WHITE);
+  if (blinking && !blinkOn) {
+    display.print(" ");
+  } else {
+    display.print(":");
+  }
+
+  
+  if (highlight == 2) {
+    display.fillRect(cursorX + 8 + wHighlight, cursorY - 2, wUnit*2 + 2, hUnit + 2, WHITE);
+    display.setTextColor(BLACK);
+  }
   if (time.minute < 10) display.print(0);
   display.print(time.minute); display.print(" ");
-  
+  if (highlight == 2) display.setTextColor(WHITE);
+
+
+  if (highlight == 3) {
+    cursorX += wUnit*3;
+    display.fillRect(cursorX + 8 + wHighlight, cursorY - 2, wUnit*2 + 2, hUnit + 2, WHITE);
+    display.setTextColor(BLACK);
+  }
   if (time.hour > 11) {
     display.print("PM");
   } else {
     display.print("AM");
   }
+  if (highlight == 3) {
+      display.setTextColor(WHITE);
+  }
+  
   display.setTextSize(1); // reset after
 }
 
@@ -121,90 +168,94 @@ void displayHomeScreen(Time nextMealTime) {
   display.clearDisplay();
   
   Time currTime = getCurrentTime();
-  displayTime(currTime, 20, 10, 2);
+  handleBlinks(9);
+  displayTime(currTime, 10, 2, 0, true);
   
-  displayPrintCenter("Next meal time:", 35, WHITE);
-  displayTime(nextMealTime, 35, 47, 1);
+  displayPrintCenter("Next meal time", 35, WHITE);
+  displayTime(nextMealTime, 47, 1, 0, false);
   
   display.display();
 }
 
 void updateButtons() {
-  uint16_t A1Val = analogRead(A1);
-  uint16_t A2Val = analogRead(A2);
-  uint16_t A3Val = analogRead(A3);
+  uint16_t UPVal = analogRead(UP_PIN);
+  uint16_t MIDVal = analogRead(MID_PIN);
+  uint16_t DOWNVal = analogRead(DOWN_PIN);
 
-  if (wasPressedA1) wasPressedA1 = false;
-  if (A1Val < 500) isDownA1 = true;
-  if (A1Val > 500 && isDownA1) {
-    isDownA1 = false;
-    wasPressedA1 = true;
+  if (wasPressedUP) wasPressedUP = false;
+  if (UPVal < 500) isDownUP = true;
+  if (UPVal > 500 && isDownUP) {
+    isDownUP = false;
+    wasPressedUP = true;
   }
 
-  if (wasPressedA2) wasPressedA2 = false;
-  if (A2Val < 500) isDownA2 = true;
-  if (A2Val > 500 && isDownA2) {
-    isDownA2 = false;
-    wasPressedA2 = true;
+  if (wasPressedMID) wasPressedMID = false;
+  if (MIDVal < 500) isDownMID = true;
+  if (MIDVal > 500 && isDownMID) {
+    isDownMID = false;
+    wasPressedMID = true;
   }
 
-  if (wasPressedA3) wasPressedA3 = false;
-  if (A3Val < 500) isDownA3 = true;
-  if (A3Val > 500 && isDownA3) {
-    isDownA3 = false;
-    wasPressedA3 = true;
+  if (wasPressedDOWN) wasPressedDOWN = false;
+  if (DOWNVal < 500) isDownDOWN = true;
+  if (DOWNVal > 500 && isDownDOWN) {
+    isDownDOWN = false;
+    wasPressedDOWN = true;
   }
 }
 
 Time incrementTime(Time time, bool up, char timeSection) {
-  if (timeSection == 'h' && up) {
+  if (timeSection == 1 && up) {
     time.hour = time.hour + 1;
     if (time.hour == 24) time.hour = 0;
   }
   
-  if (timeSection == 'h' && !up) {
+  if (timeSection == 1 && !up) {
     time.hour = time.hour - 1;
     if (time.hour == 255) time.hour = 23;
   }
 
-  if (timeSection == 'm' && up) {
+  if (timeSection == 2 && up) {
     time.minute += 1;
     if (time.minute == 60) time.minute = 0;
   }
   
-  if (timeSection == 'm' && !up) {
+  if (timeSection == 2 && !up) {
     time.minute -= 1;
     if (time.minute == 255) time.minute = 59;
+  }
+
+  if (timeSection == 3) {
+    time.hour += 12;
+    time.hour = time.hour % 24;
   }
   
   return time;
 }
 
-Time userSetTime(char* title) {
-  Time setTime = {12, 0, 0};
-  char timeSections [2] = {'h','m'};
-  uint8_t timeSectionInd = 0;
+Time userSetTime(char* title, Time setTime) {
+  uint8_t timeSection = 1;
 
   while(1) {
     updateButtons();
-    if (wasPressedA1) {
-      setTime = incrementTime(setTime, false, timeSections[timeSectionInd]);
+    if (wasPressedUP) {
+      setTime = incrementTime(setTime, false, timeSection);
     }
     
-    if (wasPressedA2) {
-      if (timeSectionInd >= 1) {
+    if (wasPressedMID) {
+      if (timeSection >= 3) {
         return setTime;
       }
-      timeSectionInd += 1;
+      timeSection += 1;
     }
     
-    if (wasPressedA3) {
-      setTime = incrementTime(setTime, true, timeSections[timeSectionInd]);
+    if (wasPressedDOWN) {
+      setTime = incrementTime(setTime, true, timeSection);
     }
     
     display.clearDisplay();
-    displayPrintCenter(title, 5, WHITE);
-    displayTime(setTime, 25, 25, 2);
+    displayPrintCenter(title, 10, WHITE);
+    displayTime(setTime, 30, 2, timeSection, false);
     display.display();
     delay(5);
   }
@@ -217,7 +268,7 @@ bool userChooseOptions(char* op1, char* op2) {
     display.clearDisplay();
     updateButtons();
 
-    if (wasPressedA1 || wasPressedA3) isHighlighted1^=true;
+    if (wasPressedUP || wasPressedDOWN) isHighlighted1^=true;
     
     if (isHighlighted1) {
       display.fillRect(BORDER, BORDER, SCREEN_WIDTH - 2*BORDER, 
@@ -236,7 +287,7 @@ bool userChooseOptions(char* op1, char* op2) {
       displayPrintCenter(op2, SCREEN_HEIGHT*3/4, BLACK);
     }
 
-    if (wasPressedA2) {
+    if (wasPressedMID) {
       return isHighlighted1;
     }
     
@@ -251,20 +302,49 @@ void printTime(Time time) {
   Serial.println(time.minute);
 }
 
+bool isAligned() {
+  int home_data = analogRead(HOME_PIN);
+  return (home_data < 500);
+}
+
+void homeTray() {
+  display.clearDisplay();
+  displayPrintCenter("Initializing...", SCREEN_HEIGHT/2, WHITE);
+  display.display();
+  stepper.setSpeedInStepsPerSecond(100*32);
+  stepper.setAccelerationInStepsPerSecondPerSecond(100*64);
+
+  while (!isAligned()) {
+    stepper.moveRelativeInSteps(200*8 / 12);
+  }
+}
+
+void rotateTray() {
+  stepper.moveRelativeInSteps(200*8 / 12 * 63);
+}
+
 void checkIfFeedTime() {
   if (getMinutes(nextMealTime) == getMinutes(getCurrentTime())) {
     Serial.println("TIME TO EAT!");
-    // rotateTray();
-    delay(1001); //TODO: don't need this once motor rotation exists
+    rotateTray();
   }
 }
 
 void setup() {
-  pinMode(A1, INPUT_PULLUP);
-  pinMode(A2, INPUT_PULLUP);
-  pinMode(A3, INPUT_PULLUP);  
-  
   Serial.begin(9600);
+  
+  pinMode(UP_PIN, INPUT_PULLUP);
+  pinMode(MID_PIN, INPUT_PULLUP);
+  pinMode(DOWN_PIN, INPUT_PULLUP); 
+  pinMode(EN_PIN, OUTPUT);
+  pinMode(M0_PIN, OUTPUT);
+  pinMode(M1_PIN, OUTPUT);
+  pinMode(M2_PIN, OUTPUT);
+  digitalWrite(EN_PIN, LOW); 
+  digitalWrite(M0_PIN, HIGH);
+  digitalWrite(M1_PIN, HIGH);
+  digitalWrite(M2_PIN, LOW); 
+
   #ifndef ESP8266
   while (!Serial); // wait for serial port to connect. Needed for native USB
   #endif
@@ -282,34 +362,44 @@ void setup() {
     Serial.println(F("Al F"));
     for(;;); // Don't proceed, loop forever
   }
+  
+  display.setTextSize(1);
+  stepper.connectToPins(MOTOR_STEP_PIN, MOTOR_DIRECTION_PIN);
+  homeTray();
+
+//  Time setTime;
+//  setTime.hour = 12;
+//  setTime.minute = 11;
+//  setTime.second = 15;
+//  setCurrentTime(setTime);
 }
 
 void loop() {
   checkIfFeedTime();
   getNextMealTime();
-  Serial.print("next "); printTime(nextMealTime);
-  Serial.print("1 "); printTime(getFeedTime(0));
-  Serial.print("2 "); printTime(getFeedTime(1));
   displayHomeScreen(nextMealTime);
   updateButtons();
 
-  if (wasPressedA1 || wasPressedA2 || wasPressedA3) {
-    bool menuSelection = userChooseOptions("Choose meal times", "Set current time");
+  if (wasPressedUP || wasPressedMID || wasPressedDOWN) {
+    bool menuSelection = userChooseOptions("Set meal times", "Set current time");
     
     if (menuSelection) {
-      bool mealSelection = userChooseOptions("Meal 1", "Meal 2");
+      bool mealSelection = userChooseOptions("First meal", "Second meal");
       
       if (mealSelection) {
-        Time meal1Time = userSetTime("Set meal 1 time");
+        char title[] = "Set first meal time";
+        Time meal1Time = userSetTime(title, getFeedTime(0));
         saveFeedTime(meal1Time, 0);
 
       } else {
-        Time meal2Time = userSetTime("Set meal 2 time");
+        char title[] = "Set second meal time";
+        Time meal2Time = userSetTime(title, getFeedTime(1));
         saveFeedTime(meal2Time, 1);
       }
       
     } else {
-      Time setTime = userSetTime("Set current time");
+      char title[] = "Set current time";
+      Time setTime = userSetTime(title, getCurrentTime());
       setCurrentTime(setTime);
     }
   }
